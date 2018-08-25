b7eb2880f3201ad22ed907c2cf7b2de3d84a4bde
SHUN <41610882+shun198914@users.noreply.github.com>
2018-08-25 15:10:51 +0900

    Add files via upload

---
+++ ClsDataFormat/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("ClsDataFormat")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("メーキュー株式会社")>
+<Assembly: AssemblyProduct("ClsDataFormat")> 
+<Assembly: AssemblyCopyright("2018 MEKYU Co.Ltd")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("9884670b-13a1-41e2-bc40-4afd8ad5131f")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+' ＜メジャー・バージョン＞.＜マイナー・バージョン＞.＜ビルド番号＞.＜リビジョン＞
+'      Major Version....全機能を一新するような大きな改修が行われた場合にインクリメント
+'      Minor Version....一部機能が追加された等の改修時にインクリメント
+'      Build Number.....リリース・ビルド毎にインクリメント。自動時は2000/1/1からの経過日数
+'      Revision.........バグ修正毎にインクリメント。自動時は0時からの経過秒数を2で割った値
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("7.0.1312.1801")> 
